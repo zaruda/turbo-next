95f0995bc2e6f1e7f392f5645ccc06e5913c1514
Ilya Zarudny <ilya.zarudny@valvespace.com>
2023-05-05 13:36:56 +0300

    Add serverside props

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,16 @@
+import { GetServerSideProps } from "next";
 import { Button } from "ui";
 
-export default function Web() {
+export default function Web(props) {
+  console.log(props)
   return (
     <div>
       <h1>Web</h1>
       <Button />
     </div>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return { props: { data: 'test' } }
 }
